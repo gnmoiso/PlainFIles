8cf2eee620df889f2aba5336c4960edad4f3711f
gnmoiso <moisoyt@gmail.com>
2025-10-23 20:25:30 -0500

    Basic PlainFiles

---
+++ PlainFIles/PlainFIles.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using PlainFIles;
+using System.ComponentModel.Design;
+
+var textFIle = new SimpleTextFile("data.txt");
+var lines = textFIle.ReadAllLines().ToList();
+var opc = string.Empty;
+
+do
+{
+    opc = Menu();
+    switch (opc)
+    {
+        case "1":
+            Console.WriteLine("Contenido del archivo:");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+            break;
+        case "2":
+            Console.Write("Ingrese la linea a agregar: ");
+            var newLine = Console.ReadLine() ?? string.Empty;
+            lines.Add(newLine);
+            break;
+        case "3":
+            textFIle.WritteAllLines(lines);
+            Console.WriteLine("Archivo guardado.");
+            break;
+        case "0":
+            textFIle.WritteAllLines(lines);
+            Console.WriteLine("Saliendo...");
+            break;
+        default:
+            Console.WriteLine("Opcion no valida.");
+            break;
+    }
+} while (opc != "0");
+
+string Menu()
+{
+    Console.WriteLine("1. Mostrar");
+    Console.WriteLine("2. Adicionar");
+    Console.WriteLine("3. Guardar");
+    Console.WriteLine("0. Sair");
+    Console.Write("su opcion es: ");
+    return Console.ReadLine() ?? string.Empty;
+}
